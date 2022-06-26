112a57748bd6fcb5cd4838b1c5515d4872f72622
Christoph Schmidt <christophschmidt37@gmail.com>
2022-06-26 12:12:00 +0200

    Project set up

---
+++ BankAnalyse.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace BankAnalyse
+{
+    partial class FormBankAnalyse
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormBankAnalyse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormBankAnalyse";
+            this.Text = "Bank Analyse";
+            this.Load += new System.EventHandler(this.FormBankAnalyse_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ BankAnalyse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankAnalyse
+{
+    public partial class FormBankAnalyse : Form
+    {
+        public FormBankAnalyse()
+        {
+            InitializeComponent();
+        }
+
+        private void FormBankAnalyse_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
